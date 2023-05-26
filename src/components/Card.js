caac7862a5bfc7e88c39539571d5494463a260ac
Monica Lara <monicalara.pineda@gmail.com>
2023-05-26 01:13:16 -0600

    Template

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <div class="card border-0" style={{width: '18rem'}}>
+            <img class="card-img-top rounded" src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="..."></img>
+        </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Menu.css'
+import Card from './Card'
+
+function Menu() {
+  return (
+    <div class='container bg-black p-4 bg-opacity-75 rounded'>
+        <Card></Card>            
+    </div>   
+  )
+}
+
+export default Menu
